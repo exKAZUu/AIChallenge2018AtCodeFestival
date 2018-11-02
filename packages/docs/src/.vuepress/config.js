11cf7e055829e6239c47bc55c358c27e4007959f
rdrgn <rdrgn@users.noreply.github.com>
2018-11-03 00:24:20 +0900

    add initial pages

---
+++ packages/docs/src/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  base: "/AIChallenge2018AtCodeFestival/",
+  title: "AI Challenge 2018 @ CODE FESTIVAL",
+  description: "対戦ゲームAIプログラミングコンテスト",
+  themeConfig: {
+    nav: [
+      { text: "応募要項", link: "/apply/" },
+      { text: "問題", link: "/question/" }
+    ],
+    search: false
+  }
+};
