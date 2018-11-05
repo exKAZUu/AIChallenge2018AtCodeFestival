e643e27cb16f05c800109efbdd4de048cffd31b7
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2018-11-05 12:46:02 +0900

    fix: refine wording and add a contact page (#11)

--- packages/docs/src/.vuepress/config.js
+++ packages/docs/src/.vuepress/config.js
@@ -1,12 +1,13 @@
 module.exports = {
   base: "/AIChallenge2018AtCodeFestival/",
   title: "AI Challenge 2018 @ CODE FESTIVAL",
-  description: "対戦ゲームAIプログラミングコンテスト",
+  description: "対戦型ゲームAIプログラミングコンテスト",
   themeConfig: {
     nav: [
       { text: "参加要項", link: "/guide/" },
-      { text: "ルール", link: "/rules/" },
-      { text: "ダウンロード", link: "/downloads/" }
+      { text: "ゲームルール", link: "/rules/" },
+      { text: "ダウンロード", link: "/downloads/" },
+      { text: "お問い合わせ", link: "/contact/" }
     ],
     search: false
   }
